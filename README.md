b868e8598121e9422ed0fbd9f5d0b449101a3fa1
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2022-01-31 17:38:58 +0300

    finds the shortest way, edit readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Knights Travails
+
+This project contains a method called #knight_moves that shows the simplest possible way to get from one square to another by outputting all squares the knight in chess will stop on along the way.
--- knights_travails.rb
+++ knights_travails.rb
@@ -1,65 +1,61 @@
-# frozen_string_literal: true
-
 class Board
+  attr_accessor :root_node
+
   def initialize
     @moves = [[1, 2], [2, 1], [-1, 2], [2, -1], [-2, -1], [-1, -2], [-2, 1], [1, -2]]
+    @history = []
+    @root_node = nil
   end
 
-  def knight_moves(position, destination, current = generate_possible_moves(position))
-    path = []
-    result = level_order(current)
-    path << current.position
-    i = 0
-    until result.include?(destination)
-      break if current.possible_moves[i].nil?
-      current = generate_possible_moves(current.possible_moves[i].position)
-      path << current.position
-      result = level_order(current)
-      i += 1
+  def knight_moves(position, destination)
+    @root_node = Knight.new(position)
+    queue = [@root_node]
+    current = queue.shift
+    @history << current.position
+    populate_possible_moves(current)
+
+    until current.position == destination
+      current.possible_moves.each do |pmove|
+        queue << pmove
+      end
+
+      current = queue.shift
+      @history << current.position
+      populate_possible_moves(current)
     end
-    path << destination
+
+    path = []
+    until current.position == @root_node.position
+      path.unshift(current.position)
+      current = current.parent
+    end
+    path.unshift(@root_node.position)
+
     puts "You made it in #{path.size - 1} moves! Here is your path: "
     path.each { |pos| p pos }
   end
 
-  def generate_possible_moves(position)
-    knight = Knight.new(position)
+  def populate_possible_moves(knight)
     @moves.each do |move|
-      if (move[0] + knight.position[0]).between?(0, 7) && (move[1] + knight.position[1]).between?(0, 7)
-        knight.possible_moves << Knight.new([knight.position[0] + move[0], knight.position[1] + move[1]])
+      next unless (move[0] + knight.position[0]).between?(0, 7) && (move[1] + knight.position[1]).between?(0, 7)
+
+      unless @history.include?(Knight.new([knight.position[0] + move[0], knight.position[1] + move[1]]).position)
+        knight.possible_moves << Knight.new([knight.position[0] + move[0], knight.position[1] + move[1]], knight)
       end
     end
-
-    knight
-  end
-
-  def level_order(current = nil, queue = [], list = [])
-    return if current.nil?
-
-    queue.push << current
-
-    until queue.empty?
-      current = queue.first
-      list.push(current.position)
-
-      8.times do |i|
-        queue.push(current.possible_moves[i]) unless current.possible_moves[i].nil?
-      end
-      queue.shift
-    end
-    list
   end
 end
 
 class Knight
-  attr_accessor :position, :possible_moves
+  attr_accessor :position, :possible_moves, :parent
 
-  def initialize(position)
+  def initialize(position, parent = nil)
     @position = position
     @possible_moves = []
+    @parent = parent
   end
 end
 
 board = Board.new
 
-board.knight_moves([3, 3], [0, 0])
+board.knight_moves([3, 3], [4, 3])
